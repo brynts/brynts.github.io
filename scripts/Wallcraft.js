79eb9b5651dd155cdab1bcbbb0e9172a0b385cd9
Bryant <bryant.havok@gmail.com>
2025-03-10 11:54:56 +0700

    Create Wallcraft.js

---
+++ scripts/Wallcraft.js
@@ -0,0 +1,78 @@
+var response = {
+  "items": {
+    "all_time": {
+      "is_active": true,
+      "type": "nonconsumable"
+    },
+    "all_time.19.5baks": {
+      "is_active": true,
+      "type": "nonconsumable"
+    },
+    "all_time.26baks": {
+      "is_active": true,
+      "type": "nonconsumable"
+    },
+    "com.wallpaperscraft.wallpapers.6months.1": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.6months.1.10baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.6months.1.7.5baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.month.1": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.month.1.1.5baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.month.1.2baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.month.1.3baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.year.1": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.year.1.13.5baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.year.1.18baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.year.1.1baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    },
+    "com.wallpaperscraft.wallpapers.year.1.5baks": {
+      "already_used_introductory_price": false,
+      "is_active": false,
+      "type": "subscription"
+    }
+  }
+};
+
+$done({ body: JSON.stringify(response) });
